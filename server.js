f604a65638162408e9a7a78f99b90e47a6e1b534
AmeenBassamSaleh <ameenbassam222@gmail.com>
2021-05-06 23:21:10 +0300

    fadi

--- server.js
+++ server.js
@@ -1,35 +1,52 @@
 require('dotenv').config();
-const express = require('express')
-const weather = require('./assets/weather.json')
-const cors = require('cors')
+const express = require('express');
+const weather = require('./assets/weather.json');
+const superagent = require('superagent');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-const PORT = process.env.PORT ||3020
+const PORT = process.env.PORT || 3020;
+const WEATHER_BIT_KEY = process.env.WEATHER_BIT_KEY;
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 });
 
-app.get('/weather', (req, res)=>{
-  const arrayOfData = weather.data.map(data => new Weather(data));
-  res.send(arrayOfData);
+app.get('/weather', getWeather)
 
-});
+function getWeather(req, res) {
+  console.log(req.query.lat);
+  console.log('her is bake end');
+  // console.log(req.query.lat);
+
+  const weatherUrl = (`https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${req.query.lat}&lon=${req.query.lon}`);
+  // const weatherUrl = (`https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=-23.62153185&lon=-46.6799024176782`);
+
+  superagent.get(weatherUrl).then(arrayOfData => {
+    // console.log(arrayOfData.body);
+    console.log(arrayOfData.body.data[0]);
+
+    const arrayOf = arrayOfData.body.data.map(data => new Weather(data));
+    res.send(arrayOf); // sent to front
+  }).catch(console.error)
+  
+  // console.log(weatherUrl);
+  console.log(req.query.name);
+
+}
+
 
 class Weather {
-  constructor(data){
+  constructor(data) {
     this.date = data.valid_date;
     this.description = data.weather.description;
   }
 }
- 
-app.listen(PORT,()=>console.log('app is lisening on port ' +PORT))
 
-
-
+app.listen(PORT, () => console.log('app is lisening on port ' + PORT))
 
 
 
